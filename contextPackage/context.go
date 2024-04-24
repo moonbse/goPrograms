812c39fa635936ca67bc5ef1201a03a311a0a3e6
Nirbhay11 Singh <nirbhay11.Singh@ril.com>
2024-04-24 19:06:58 +0530

    created this module for creating go projects while learning

--- contextPackage/context.go
+++ contextPackage/context.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+)
+// using colly to build this scraper, it can be used to build crawlers or spiders
+// Colly is an open-source library that provides a clean interface based on callbacks to
+// write a scraper, crawler or spider. It comes with an advanced Go web scraping API that allows you to download an HTML page,
+//  automatically parse its content, select HTML elements from the DOM and retrieve data from them.
+
 
 func main() {
 
