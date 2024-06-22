d78db138753d236df997435c7790bd6191730101
Nirbhay11 Singh <oneworld191@gmail.com>
2024-06-22 20:19:30 +0530

    changes

--- structureMicroService/main.go
+++ structureMicroService/main.go
@@ -1,25 +1,49 @@
 package main
 
 import (
-	"context"
+	// "context"
 	"fmt"
-	"log"
+	"strings"
+
+	// "log"
+	"os"
 )
 
 func main() {
 
-	svc := NewCatFactService("https://catfact.ninja/fact")
-	svc = newLoggingService(svc)
+	// svc := NewCatFactService("https://catfact.ninja/fact")
+	// svc = newLoggingService(svc)
 
-	fact, err := svc.getCatFact(context.TODO())
+	// fact, err := svc.getCatFact(context.TODO())
 
-	if err != nil {
-		log.Fatal(err)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// fmt.Printf("%+v\n", fact)
+
+	// apiServer := newApiServer(svc)
+	// log.Fatal(apiServer.Start(":3001"))
+
+	var s, sep string
+
+	for i := 1; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
 	}
 
-	fmt.Printf("%+v\n", fact)
+	fmt.Println(s)
 
-	apiServer := newApiServer(svc)
-	log.Fatal(apiServer.Start(":3001"))
+	s = ""
+	sep = ""
+
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+
+	fmt.Println(s)
+
+	fmt.Println(strings.Join(os.Args[1:len(os.Args)], " "))
 
 }
